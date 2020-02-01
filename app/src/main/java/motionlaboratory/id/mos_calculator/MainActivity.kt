bd6997101fc3bb8273a63ece0c89878686f06968
maqielhilmanm <maqiel2600@gmail.com>
2020-02-01 11:40:22 +0700

    first commit

---
+++ app/src/main/java/motionlaboratory/id/mos_calculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package motionlaboratory.id.mos_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
